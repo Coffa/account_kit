cda1ee5abdeb5a73e9b83a5bcb70d24d862978c0
Tan <tan@ring.md>
2016-04-13 23:34:39 +0800

    remove failed test

--- spec/account_kit_spec.rb
+++ spec/account_kit_spec.rb
@@ -4,8 +4,4 @@ describe AccountKit do
   it 'has a version number' do
     expect(AccountKit::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end

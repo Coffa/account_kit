9802bbca075976b59e38fcc81e3a2ed788bc9f53
MQuy <sugiacupit@gmail.com>
2016-04-15 14:06:03 +0800

    Config shorten

--- lib/account_kit.rb
+++ lib/account_kit.rb
@@ -3,6 +3,20 @@ require 'json'
 
 require 'account_kit/version'
 require 'account_kit/api'
+require 'accountkit/config'
 
 module AccountKit
+  extend self
+
+  def configure
+    yield Config
+  end
+
+  def access_token(code)
+    API.access_token(code)
+  end
+
+  def me(access_token)
+    API.me(access_token)
+  end
 end
